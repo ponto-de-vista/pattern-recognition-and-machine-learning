abba7b738414c4dedd8e93544542947d3e2214aa
Leonardo <next4.leonardo@gmail.com>
2026-04-20 22:20:52 -0300

    add codigo notebook

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import pandas as pd
 import dask.dataframe as dd
 
 # Load the CSV file
-df = dd.read_csv('.\despesas-2008\despesas-2008.csv',
+df = dd.read_csv('despesas-2025.csv',
                  encoding='latin1',
                  on_bad_lines='skip',
                  sep=';',
-                 assume_missing=True,
-                 low_memory=False)
+                 assume_missing=True)
 
 # View the first 5 rows
 print(df.head())
