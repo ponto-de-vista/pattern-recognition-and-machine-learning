264414d87234c480b69265f2ad1f8839f41311d5
Bryan Pinheiro <40837519+bryanpinheiro@users.noreply.github.com>
2026-04-20 21:28:08 -0300

    feat: add example

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello WORLD!")
+import pandas as pd
+import dask.dataframe as dd
+
+# Load the CSV file
+df = dd.read_csv('.\despesas-2008\despesas-2008.csv',
+                 encoding='latin1',
+                 on_bad_lines='skip',
+                 sep=';',
+                 assume_missing=True,
+                 low_memory=False)
+
+# View the first 5 rows
+print(df.head())
+print(len(df))
